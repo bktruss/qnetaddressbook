2cf5f9bc6bed1b2193df8da2686dfff9064d44ac
lorenxo86 <lorenxo86@ed668b86-8ca1-11dd-86ef-f1224e8fcbc0>
2008-09-27 15:57:31 +0000

    Initial commit.

---
+++ trunk/QNetAddressBook.pro
@@ -0,0 +1,76 @@
+TEMPLATE = app
+TARGET = QNetAddressBook
+VERSION = 0.0.1
+CONFIG += release warn_on
+QT += network sql
+DESTDIR = bin
+MOC_DIR = build
+OBJECTS_DIR = build
+RCC_DIR = build
+UI_DIR = build
+
+VERSIONSTR = '\\"$${VERSION}\\"'
+DEFINES += VERSION=\"$${VERSIONSTR}\"
+
+DEPENDPATH += QMapControl \
+ resources \
+ src \
+ ui
+
+INCLUDEPATH += QMapControl src
+
+HEADERS += QMapControl/circlepoint.h \
+ QMapControl/curve.h \
+ QMapControl/geometry.h \
+ QMapControl/geometrylayer.h \
+ QMapControl/googlemapadapter.h \
+ QMapControl/googlesatmapadapter.h \
+ QMapControl/gps_position.h \
+ QMapControl/imagemanager.h \
+ QMapControl/imagepoint.h \
+ QMapControl/layer.h \
+ QMapControl/layermanager.h \
+ QMapControl/linestring.h \
+ QMapControl/mapadapter.h \
+ QMapControl/mapcontrol.h \
+ QMapControl/maplayer.h \
+ QMapControl/mapnetwork.h \
+ QMapControl/osmmapadapter.h \
+ QMapControl/point.h \
+ QMapControl/tilemapadapter.h \
+ QMapControl/wmsmapadapter.h \
+ QMapControl/yahoomapadapter.h \
+ src/mainwindow.h \
+ src/centralwidget.h \
+ src/networkdialog.h
+
+SOURCES += QMapControl/circlepoint.cpp \
+ QMapControl/curve.cpp \
+ QMapControl/geometry.cpp \
+ QMapControl/geometrylayer.cpp \
+ QMapControl/googlemapadapter.cpp \
+ QMapControl/googlesatmapadapter.cpp \
+ QMapControl/gps_position.cpp \
+ QMapControl/imagemanager.cpp \
+ QMapControl/imagepoint.cpp \
+ QMapControl/layer.cpp \
+ QMapControl/layermanager.cpp \
+ QMapControl/linestring.cpp \
+ QMapControl/mapadapter.cpp \
+ QMapControl/mapcontrol.cpp \
+ QMapControl/maplayer.cpp \
+ QMapControl/mapnetwork.cpp \
+ QMapControl/osmmapadapter.cpp \
+ QMapControl/point.cpp \
+ QMapControl/tilemapadapter.cpp \
+ QMapControl/wmsmapadapter.cpp \
+ QMapControl/yahoomapadapter.cpp \
+ src/main.cpp \
+ src/mainwindow.cpp \
+ src/centralwidget.cpp \
+ src/networkdialog.cpp
+ 
+FORMS += ui/mainwindow.ui ui/networkdialog.ui
+
+RESOURCES += resources/resources.qrc
+
---
+++ trunk/src/centralwidget.cpp
@@ -0,0 +1,221 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Lorenzo Masini                                  *
+ *   lorenxo86@gmail.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include <QVBoxLayout>
+#include <QPen>
+#include <QColor>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QTextStream>
+#include <QIODevice>
+#include <QStringList>
+
+#include "centralwidget.h"
+#include "googlemapadapter.h"
+#include "maplayer.h"
+#include "geometrylayer.h"
+#include "geometry.h"
+#include "circlepoint.h"
+
+#include "networkdialog.h"
+
+CentralWidget::CentralWidget( QWidget *parent ) 
+	: QWidget(parent)
+{
+	control = new MapControl(QSize(600, 600));
+	control->enablePersistentCache();
+	adapter = new GoogleMapAdapter();
+	base = new MapLayer("base", adapter);
+	control->addLayer(base);
+	
+	layers[None] = new GeometryLayer("None", adapter);
+	layers[WEP] = new GeometryLayer("WEP", adapter);
+	layers[WPA] = new GeometryLayer("WPA", adapter);
+	layers[WPA2] = new GeometryLayer("WPA2", adapter);
+	
+	for(int i = 0; i<=3; i++)
+		control->addLayer(layers[i]);
+
+	/* Initial view */
+	control->setView(QPointF(0,0));
+	control->setZoom(2);
+	
+	QGridLayout *layout = new QGridLayout(this);
+	layout->addWidget(control, 0, 0, Qt::AlignHCenter | Qt::AlignVCenter);
+	setLayout(layout);
+	control->setMinimumSize(QSize(601,601));
+}
+
+void CentralWidget::loadNetworks()
+{
+	QSqlQuery query;
+	if(!query.exec("SELECT bssid, lat, lon, encryption FROM networks"))
+		return;
+	while(query.next()){
+		QString bssid = query.value(0).toString();
+		qreal lat = query.value(1).toDouble();		
+		qreal lon = query.value(2).toDouble();
+		int encryption = query.value(3).toInt();
+		addNetwork((NetworkEncryption)encryption, lon, lat, bssid);
+	}
+}
+
+void CentralWidget::importNetworks(QIODevice &device)
+{
+	QTextStream stream(&device);
+	
+	while(!stream.atEnd()){
+		QString line = stream.readLine();
+		importNetwork(line);
+	}
+	clearNetworks();
+	loadNetworks();	
+}
+
+// TODO: Do not permit to insert empty networks!
+void CentralWidget::addNetwork()
+{
+	QPointF coordinate = control->currentCoordinate();
+	NetworkDialog dialog(this);
+	
+	dialog.setLatitude(coordinate.y());
+	dialog.setLongitude(coordinate.x());
+
+	if(dialog.exec()){
+		QSqlQuery writeQuery;
+		writeQuery.prepare("INSERT INTO networks (essid,bssid,channel,signal,lat,lon,password,comment,encryption) VALUES (:essid,:bssid,:channel,:signal,:latitude,:longitude,:comment,:encryption)");	
+		writeQuery.bindValue(":bssid", dialog.bssid());
+		writeQuery.bindValue(":essid", dialog.essid());
+		writeQuery.bindValue(":channel", dialog.channel());
+		writeQuery.bindValue(":signal", dialog.signal());
+		writeQuery.bindValue(":latitude", dialog.latitude());
+		writeQuery.bindValue(":longitude", dialog.longitude());
+		writeQuery.bindValue(":comment", dialog.comment());
+		writeQuery.bindValue(":encryption", dialog.encryption());
+		if(!writeQuery.exec())
+			return;	// Error handling here.
+			
+		clearNetworks();
+		loadNetworks();
+	}
+}
+
+void CentralWidget::clearNetworks()
+{
+	for(int i = 0; i <= 3; i++)
+		layers[i]->clearGeometries();
+	control->updateRequestNew();
+}
+
+void CentralWidget::showNetwork(Geometry *geometry, QPoint /*point*/)
+{
+	QSqlQuery query;
+	query.prepare("SELECT * from networks WHERE bssid=:bssid");
+	query.bindValue(":bssid", geometry->name());
+	if(!query.exec())
+		return; // Error handling here.
+
+	query.next();
+	NetworkDialog dialog(this);
+	dialog.setEssid(query.value(0).toString());
+	dialog.setBssid(query.value(1).toString());
+	dialog.setChannel(query.value(2).toInt());
+	dialog.setSignal(query.value(3).toInt());
+	dialog.setLatitude(query.value(4).toDouble());
+	dialog.setLongitude(query.value(5).toDouble());
+	dialog.setComment(query.value(6).toString());
+	dialog.setEncryption(query.value(7).toInt());
+	
+	if(dialog.exec()){
+		QSqlQuery writeQuery;
+		writeQuery.prepare("UPDATE networks SET essid=:essid, bssid=:bssid, channel=:channel, signal=:signal, lat=:latitude, lon=:longitude, comment=:comment, encryption=:encryption WHERE bssid=:bssidprev");	
+		writeQuery.bindValue(":bssid", dialog.bssid());
+		writeQuery.bindValue(":essid", dialog.essid());
+		writeQuery.bindValue(":channel", dialog.channel());
+		writeQuery.bindValue(":signal", dialog.signal());
+		writeQuery.bindValue(":latitude", dialog.latitude());
+		writeQuery.bindValue(":longitude", dialog.longitude());
+		writeQuery.bindValue(":comment", dialog.comment());
+		writeQuery.bindValue(":encryption", dialog.encryption());
+		writeQuery.bindValue(":bssidprev", geometry->name());
+		if(!writeQuery.exec())
+			return;	// Error handling here.
+		clearNetworks();
+		loadNetworks();
+	}
+}
+
+void CentralWidget::importNetwork(const QString &line)
+{
+	QStringList fields = line.split(";");
+	NetworkEncryption encryption = None;
+
+	if(fields.at(1) != "infrastructure")
+		return;
+		
+	if(fields.at(2) == "<no ssid>")
+		return;
+	
+	if(fields.at(7).contains("None"))
+		encryption = None;
+	
+	if(fields.at(7).contains("WEP"))
+		encryption = WEP;
+		
+	if(fields.at(7).contains("TKIP"))
+		encryption = WPA;
+		
+	if(fields.at(7).contains("AES-CCM"))
+		encryption = WPA2;
+			
+	QSqlQuery query;
+	query.prepare("INSERT INTO networks (essid,bssid,channel,signal,lat,lon,comment,encryption) VALUES (:essid,:bssid,:channel,:signal,:lat,:lon,NULL,:encryption)");
+	query.bindValue(":essid", fields.at(2));
+	query.bindValue(":bssid", fields.at(3));
+	query.bindValue(":channel", fields.at(5));
+	query.bindValue(":signal", fields.at(22));
+	query.bindValue(":lat", fields.at(32));
+	query.bindValue(":lon", fields.at(33));
+	query.bindValue(":encryption", encryption);
+	query.exec(); // Error handling here		
+}
+
+void CentralWidget::addNetwork(NetworkEncryption encryption, qreal x, qreal y, QString name)
+{
+	QPen pen;
+	switch(encryption){
+		case None:
+			pen.setColor(QColor(Qt::red));
+			break;
+		case WEP:
+			pen.setColor(QColor(Qt::yellow));
+			break;
+		case WPA:
+			pen.setColor(QColor(Qt::blue));
+			break;
+		case WPA2:
+			pen.setColor(QColor(Qt::green));	
+	}			
+	pen.setWidth(3);
+		
+	CirclePoint *network = new CirclePoint(x, y, 15, name, qmapcontrol::Point::Middle, &pen);
+	connect(network, SIGNAL(geometryClicked(Geometry *, QPoint)), this, SLOT(showNetwork(Geometry *, QPoint)));	
+	layers[encryption]->addGeometry(network);
+}
+
